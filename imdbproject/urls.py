503ba35eb055f0ff332a7eca4a02ff1c94ecd2a1
Nuthan <nuthanhsn@gmail.com>
2021-02-23 20:58:49 +0530

    df

--- imdbproject/urls.py
+++ imdbproject/urls.py
@@ -23,8 +23,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homeview, name="home"),
-    path('result/', resultview, name="result"),
-
-     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('result/', resultview, name="result")
 ]
