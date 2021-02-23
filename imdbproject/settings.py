a94638d32bb49b3644512cbbd811155c2d4c28bf
Nuthan <nuthanhsn@gmail.com>
2021-02-23 20:35:42 +0530

    static

--- imdbproject/settings.py
+++ imdbproject/settings.py
@@ -15,6 +15,11 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 
